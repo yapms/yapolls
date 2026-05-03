f3c91ae60bf07309229816d14a919be230015f23
Jacob Enders <jacobenders1@gmail.com>
2026-05-03 12:20:23 -0400

    setup sveltekit from commandline

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+	description = "YAPolls";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem(
+		system: let
+			pkgs = nixpkgs.legacyPackages.${system};
+		in {
+			devShells.default = pkgs.mkShellNoCC {
+				buildInputs = [
+					pkgs.nodejs_25
+				];
+				shellHook = ''
+					echo "Welcome to the YAPolls development environment!"
+				'';
+			};
+		}
+	);
+}
---
+++ yapolls/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>YAPolls</h1>
+
+<p>This is an experimental website to aggregate polling data.</p>
